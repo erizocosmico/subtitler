99c56af24556c96aa4d106e898c828cd5da47897
Miguel Molina <hi@mvader.me>
2016-01-22 15:38:44 +0100

    Initial Commit

---
+++ Subtitler/Subtitler.h
@@ -0,0 +1,19 @@
+//
+//  Subtitler.h
+//  Subtitler
+//
+//  Created by Miguel on 22/01/16.
+//  Copyright © 2016 mvader. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Subtitler.
+FOUNDATION_EXPORT double SubtitlerVersionNumber;
+
+//! Project version string for Subtitler.
+FOUNDATION_EXPORT const unsigned char SubtitlerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Subtitler/PublicHeader.h>
+
+
